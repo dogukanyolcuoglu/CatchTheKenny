3974bbff31e194a40822950e51f74ff83deb988d
dogukanyolcuoglu <dogukan.yolcuoglu09@gmail.com>
2020-11-09 14:09:52 +0300

    Initial Commit

---
+++ KennyCatchApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KennyCatchApp
+//
+//  Created by Dogukan Yolcuoglu on 9.11.2020.
+//  Copyright © 2020 Dogukan Yolcuoglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
